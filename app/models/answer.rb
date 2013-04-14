bd04e6760a455fd0e5c74ac082674d3d7d6e5787
Janny <uspex-uspet@yandex.ru>
2013-04-14 18:11:04 +0300

    Add answer ordering by rating.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,6 +5,10 @@ class Answer < ActiveRecord::Base
 	belongs_to :question
   has_many :votes, as: :votable
 
+  scope :ordered_by_rating, select("answers.*, IFNULL(Sum(votes.rating), 0) as rating").
+                      joins("LEFT JOIN `votes` ON `votes`.`votable_id` = `answers`.`id` AND `votes`.`votable_type` = 'Answer'").
+                      group("answers.id").order("rating DESC")
+
 	validates :text, presence: true
 	validates :user, presence: true
 	validates :question, presence: true
